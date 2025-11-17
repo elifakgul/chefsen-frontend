f983af2050a744400474df4383c98025bf88a47f
elifakgul <95911454+elifakgul@users.noreply.github.com>
2025-11-17 23:46:24 +0300

    Fix: correct tarifler.css path

--- src/pages/Tarifler.jsx
+++ src/pages/Tarifler.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
-import "./tarifler.css";
+import "./Tarifler.css";
 
 function Tarifler() {
   const [recipes, setRecipes] = useState([]);
